c535944f029e9a311c2a5d772287afc68e639f59
Moataz Abd Elwhab <moatazAbdElwhab@gmail.com>
2025-04-09 23:04:45 +0300

    create files

---
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('Profile'),
+          ],
+        ),
+      ],
+    );
+  }
+}
